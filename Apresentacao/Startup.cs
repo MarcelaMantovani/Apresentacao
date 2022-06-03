9f5437a753b0987a3aa807f7d2fe274139d22079
MARCELA MANTOVANI <mantovani1@live.com>
2022-06-02 21:44:06 -0300

    Apresentação

--- Apresentacao/Startup.cs
+++ Apresentacao/Startup.cs
@@ -1,14 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Apresentacao
 {
@@ -26,6 +21,7 @@ namespace Apresentacao
         {
             services.AddControllersWithViews();
             services.AddDbContext<Contexto>(a => a.UseMySQL("server=localhost;database=BancoProjeto;user=root;password=;SslMode=none"));
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
